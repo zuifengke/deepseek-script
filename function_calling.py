090451fc4deb4136b737b3a8a90fcf406f8e6672
yehui <yehui1989@qq.com>
2025-03-17 09:59:59 +0800

    fix: 中文气温描述,如果使用deepseek-chat模型，在发起新的对话时传入了tools参数，会导致生成的回复中再一次出现函数调用的结果。

--- function_calling.py
+++ function_calling.py
@@ -1,5 +1,6 @@
 import json
 import os
+from json.decoder import JSONObject
 from pyexpat import model
 from turtle import mode
 from dotenv import load_dotenv
@@ -32,7 +33,7 @@ def get_weather(location):
     return f"{location} 的天气预报如下：晴天，气温 32摄氏度."
 
 def get_api_key(model_name="deepseek-chat"):
-    if(model_name == "deepseek-chat"):
+    if(model_name == "deepseek-chat"  or model_name == "deepseek-rea"):
         return os.getenv("DEEPSEEK_API_KEY")
     else:
         return os.getenv("GPT_API_KEY")
@@ -46,12 +47,18 @@ def create_openai_client():
 
 
 
-def send_message(messages):
-    response = client.chat.completions.create(
-        model=model_name,
-        messages=messages,
-        tools=tools,
-    )
+def send_message(messages, use_tools = False):
+    if(use_tools):
+        response = client.chat.completions.create(
+            model=model_name,
+            messages=messages,
+            tools=tools
+        )
+    else :
+        response = client.chat.completions.create(
+            model=model_name,
+            messages=messages,
+        )
     return response.choices[0].message
 
 
@@ -59,16 +66,19 @@ def send_message(messages):
 if __name__ =='__main__':
     client = create_openai_client()
     messages = [{'role':'user','content':'今天杭州的天气如何?'}]
-    message = send_message(messages)
+    message = send_message(messages,True)
     tool = message.tool_calls[0]
     messages.append(message)
     if(len(message.tool_calls)>0):
         location = json.loads(message.tool_calls[0].function.arguments).get("location")
         function_name = message.tool_calls[0].function.name
         if(function_name == "get_weather"):
-            weather_info = get_weather(location)
+            # weather_info = get_weather(location)
+            weather_info="{'温度':'25°C', '湿度':'80%','天气':'多云'}"
+            # 将 weather_info 转换为 JSON 字符串
+           # weather_info_str = json.dumps(weather_info, ensure_ascii=False)
             messages.append({"role": "tool", "tool_call_id": tool.id, "content": weather_info})
             message = send_message(messages)
             messages.append(message)
-            print(message)
+            print(message.content)
     print(messages)
